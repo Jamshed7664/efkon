df5cbd6a905baf4aee5fb3747317614774350d38
jamshed <jamshed7664@gmail.com>
2020-11-22 20:09:14 +0530

    Efkon project

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { AuthguardGuard } from './authguard.guard';
+import { CreateProjectServiceService } from './main/create-project/create-project-service.service';
+import { ForgetPsswordComponent } from './forget-pssword/forget-pssword.component';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    ForgetPsswordComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
+    
+
+  ],
+  providers: [AuthguardGuard,CreateProjectServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
